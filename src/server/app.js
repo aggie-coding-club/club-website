39a13b3e1c344c324d91a373148f58bca497597f
Gabriel Britain <quontas@gmail.com>
2017-11-13 13:58:26 -0600

    Created events routes and modified GETs to send API data.

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,12 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
 var github = require('./github-helper');
-var calendar = require('./calendar-helper');
+var googleHelper = require('./google-helper/');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 github.initializeClubData().then((clubData) => {
-    calendar.initCalendar((calendar) => {
-        app.use('/', require('./routes')());
+    googleHelper.calendarHelper.initCalendar((calendar) => {
+        app.use('/', require('./routes')(calendar, clubData));
     });
 });
 app.listen(80);
--- src/server/routes/events/index.js
+++ src/server/routes/events/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var events_new = require('./new');
 var events_delete = require('./delete');
 var events_update = require('./update');
-module.exports = function () {
+module.exports = function (calendar) {
+    router.get('/', (req, res) => {
+        res.json(calendar);
+    });
     router.post('/new', events_new);
     router.post('/delete', events_delete);
     router.post('/update', events_update);
