401c39ba12701da83692e08df0c34963e2a383c1
Jonathan Innis <jonathan.innis.ji@gmail.com>
2018-02-18 16:34:52 -0600

    Updated developer readme

--- src/client/club-website/src/ProjectCard.js
+++ src/client/club-website/src/ProjectCard.js
@@ -58,16 +58,16 @@ class ProjectCard extends Component{
   render(){
     return(
       <div className="projectTile" style={{backgroundColor: "#ffffff", color: "#333e44", margin: 20, width: 400, height: 300}}>
-        <div style={{position: "relative", left: 0, top: 0, width: 500, height: "auto", borderBottom: "2px solid #333e44" , margin: 0}}>
+        <div className="projectTileOverlay" style={{position: "relative", left: 0, top: 0, width: 500, height: "auto", borderBottom: "2px solid #333e44" , margin: 0}}>
           <div className="chevron"></div>
-          <h1 style={{position: "relative", left: 5, top: 0, margin: 0, width: 400, fontWeight: "1000", textAlign: "left", padding: "10px", zIndex: 2, opacity: 1, fontSize: "2.25em"}}>{this.state.title}</h1>
+          <h1 className="projectTileTitle" style={{position: "relative", left: 5, top: 0, margin: 0, width: 400, fontWeight: "1000", textAlign: "left", padding: "10px", zIndex: 2, opacity: 1, fontSize: "2.25em"}}>{this.state.title}</h1>
         </div>
-        <h5 style={{position:"relative", left: 5, fontWeight: 300, margin:0, paddingLeft: 10, paddingRight: 10, paddingTop: 10, paddingBottom: 5, fontSize: "1.25em",textAlign: "left", width: "auto"}}><b>Project Manager:</b> {this.state.projectManager}</h5>
-        {this.state.description !== '' ? <p style={{position: "relative", fontWeight: 100, fontSize: "1em", paddingLeft: 15, paddingRight: 15, paddingTop: 5, paddingBottom: 5, textAlign: "left", margin: 0}}>{this.state.description}</p> : null}
-        {this.state.members.length > 0 ? <div style={{position: "relative", textAlign: "left", paddingLeft: 15, paddingRight: 15, zIndex: 4, marginTop: 5, width: 370}}><b>Contributors: </b>{this.state.members.map(member=><Chip style={{display: "inline-flex", color: "#ffffff", backgroundColor: "#0288D1", height: 20, margin: 2, verticalAlign: "center"}} key={member.id} onClick={this.handleChipClick(member.avatar_url)} label={member.login}/>)}</div> : null}
-        {this.state.color !== '' ? <p style={{color: this.state.color, fontFamily: "serif", fontSize: "4em", margin: 0, position: "absolute", left: 5, bottom: -18}}> &middot; </p> : null}
-        <p style={{margin: 0, fontSize: "0.8em", position: "absolute", left: 23, bottom: 11.5}}> {this.state.language} </p>
-        <a target="_blank" rel="noopener noreferrer" href={this.state.githubLink} style={{color: "#333e44", textDecoration: "none"}}><FaGithub style={{position: "absolute", width: 50, height: 50, right: 10, bottom: 10, zIndex: 10}}/></a>
+        <h5 className="projectTileSubtitle" style={{position:"relative", left: 5, fontWeight: 300, margin:0, paddingLeft: 10, paddingRight: 10, paddingTop: 10, paddingBottom: 5, fontSize: "1.25em",textAlign: "left", width: "auto"}}><b>Project Manager:</b> {this.state.projectManager}</h5>
+        {this.state.description !== '' ? <p className="projectTileDescription" style={{position: "relative", fontWeight: 100, fontSize: "1em", paddingLeft: 15, paddingRight: 15, paddingTop: 5, paddingBottom: 5, textAlign: "left", margin: 0}}>{this.state.description}</p> : null}
+        {this.state.members.length > 0 ? <div className="projectTileChipsContainer" style={{position: "relative", textAlign: "left", paddingLeft: 15, paddingRight: 15, zIndex: 4, marginTop: 5, width: 370}}><b>Contributors: </b>{this.state.members.map(member=><Chip className="projectTileChips" style={{display: "inline-flex", color: "#ffffff", backgroundColor: "#0288D1", height: 20, margin: 2, verticalAlign: "center"}} key={member.id} onClick={this.handleChipClick(member.avatar_url)} label={member.login}/>)}</div> : null}
+        {this.state.color !== '' ? <p className="projectTileMiddot" style={{color: this.state.color, fontFamily: "serif", fontSize: "4em", margin: 0, position: "absolute", left: 5, bottom: -18}}> &middot; </p> : null}
+        <p className="projectTileLanguage" style={{margin: 0, fontSize: "0.8em", position: "absolute", left: 23, bottom: 11.5}}> {this.state.language} </p>
+        <a target="_blank" rel="noopener noreferrer" href={this.state.githubLink} style={{color: "#333e44", textDecoration: "none"}}><FaGithub className="projectTileGithubLink" style={{position: "absolute", width: 50, height: 50, right: 10, bottom: 10, zIndex: 10}}/></a>
       </div>
     );
   }
