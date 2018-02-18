340d9adbc322a001c8db16e6efad44405417a9a3
Jonathan Innis <jonathan.innis.ji@gmail.com>
2018-02-18 16:34:52 -0600

    Updated developer readme

--- src/client/club-website/src/EventCard.js
+++ src/client/club-website/src/EventCard.js
@@ -6,10 +6,12 @@ class EventCard extends Component{
       <div className="card">
         <div className="content">
           <div style={{fontSize: "2em"}} className="front">
-            <p style={{width: 260, paddingLeft: 20, paddingRight: 20, paddingTop: 80, fontWeight: 700, margin: 0}}> {this.props.title} </p>
-            <p style={{width: 260, paddingLeft: 20, paddingRight: 20, margin: 0, fontSize: "0.65em"}}> {this.props.location} </p>
-            <p style={{width: 260, paddingLeft: 20, paddingRight: 20, paddingTop: 30, margin: 0}}> {this.props.date} </p>
-            <p style={{width: 260, paddingLeft: 20, paddingRight: 20, margin: 0, fontSize: "0.6em"}}> {this.props.time} </p>
+            <p className="contentWidth" style={{width: 260, paddingLeft: 20, paddingRight: 20, paddingTop: 30, fontWeight: 700, margin: 0}}> {this.props.title} </p>
+            <p className="contentWidth" style={{width: 260, paddingLeft: 20, paddingRight: 20, margin: 0, fontSize: "0.65em"}}> {this.props.location} </p>
+            <div style={{position: "absolute", bottom: 30, height: "auto"}}>
+              <p className="contentWidth" style={{width: 260, paddingLeft: 20, paddingRight: 20, paddingTop: 30, margin: 0}}> {this.props.date} </p>
+              <p className="contentWidth" style={{width: 260, paddingLeft: 20, paddingRight: 20, margin: 0, fontSize: "0.6em"}}> {this.props.time} </p>
+            </div>
           </div>
           <div style={{fontSize: "2em"}} className="back">
             <p style={{position: "absolute", fontSize: "0.6em"}}> {this.props.description} </p>
