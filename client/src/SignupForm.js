d2b2e855344b5845953d903828587b2ad3bed0b5
privateryan1111 <ryanpfeifer1111@gmail.com>
2018-04-06 21:44:09 -0500

    base file added

---
+++ client/src/SignupForm.js
@@ -0,0 +1,13 @@
+import { Form, Text, Select, Submit, DynamicSubmit, IBaseValidator, Checkbox, Radio, RadioContainer } from 'guestbell-forms';
+import React, {Component} from 'react';
+
+class SignupForm extends Component{
+    /*render(){
+        return(
+
+
+        );
+    }*/
+}
+
+export default SignupForm;
