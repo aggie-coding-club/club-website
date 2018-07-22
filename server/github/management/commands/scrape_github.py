b21304c2474062a81ae3081336960bf161351b4a
SaltyQuetzals <quontas@gmail.com>
2018-07-22 16:26:39 -0500

    Added credentials file.

--- server/github/management/commands/scrape_github.py
+++ server/github/management/commands/scrape_github.py
@@ -5,9 +5,9 @@ import requests
 from django.core.management.base import BaseCommand, CommandError
 from github import models as github_models
 
-
+CREDENTIALS_PATH = os.path.abspath('/server/github/management/commands/config.json')
+def load_credentials(self):
 class Command(BaseCommand):
-    
+
     def handle(self, *args, **options):
         """Scrapes GitHub data for member, team, and repository information."""
-        
