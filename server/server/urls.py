7a787d985b69ac526573657f26b1363a09a1caf9
SaltyQuetzals <quontas@gmail.com>
2018-07-29 17:38:21 -0500

    Github app is erroring out. Removing it from the listed URLs in server/urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls')),
-    path('github/', include(github_urls)),
 ]
