ea9e3d535709298dc1b7c04125d61ce9a849480a
SaltyQuetzals <quontas@gmail.com>
2018-08-03 21:55:41 -0500

    Made Github fields read-only in admin site, and changed the plural name of Repository to "Repositories", not "Repositorys"

--- server/github/admin.py
+++ server/github/admin.py
@@ -6,14 +6,17 @@ from github import models as github_models
 
 @admin.register(github_models.GithubUser)
 class GithubUserAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ['user', 'avatar_url', 'bio', 'company',
+                       'email', 'name', 'url', 'login', 'teams', 'repositories']
 
 
 @admin.register(github_models.Repository)
 class RepositoryAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ['description', 'homepage_url',
+                       'is_private', 'name', 'primary_language', 'url']
 
 
 @admin.register(github_models.Team)
 class TeamAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ['avatar_url', 'combined_slug',
+                       'description', 'name', 'parent_team', 'url']
--- server/github/models.py
+++ server/github/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import models as auth_models
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 from rest_framework.authtoken.models import Token
 
 
@@ -26,6 +27,9 @@ class Repository(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = _('Repositories')
 
 
 class Team(models.Model):
