db4fa79116238555fa9815f1d9ebdc782899d45b
Gabriel Britain <quontas@gmail.com>
2018-08-13 14:28:32 -0500

    Created notifications models.

---
+++ server/notifications/tests/models_tests/__init__.py
@@ -0,0 +1,5 @@
+from .emailnotification_tests import EmailNotificationTests
+from .slacknotification_tests import SlackNotificationTests
+from .slackemailnotification_tests import SlackEmailNotificationTests
+
+__all__ = ['EmailNotificationTests', 'SlackNotificationTests', 'SlackEmailNotificationTests']
