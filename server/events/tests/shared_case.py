106dfb90374b2d7c4a22725d26af369c2fd7fcf3
edgar-martinez <edgarmtz97@tamu.edu>
2018-08-15 15:53:14 -0500

    Improved test clarity and fixed imports.

--- server/events/tests/shared_case.py
+++ server/events/tests/shared_case.py
@@ -1,24 +1,38 @@
-from datetime import timedelta
+import datetime
+
 from django.utils import timezone
-from django.test import TestCase
+from django.test import testcases
 from django.contrib.auth import models as auth_models
 
-class SharedTestCase(TestCase):
-    """ A base test module that sets up dummy variables for testing. """
+from events import models as event_models
+
+class SharedTestCase(testcases.TestCase):
+    """A sublcass of Django's TestCase that reduces the amount of rewriting needed for each test."""
 
     @classmethod
-    def setUpTestData(self):
-        # users
-        self.user1 = auth_models.User.objects.create_user(
-            username = 'user1', email='dragonboi33@user1.com')
-        self.user2 = auth_models.User.objects.create_user(
-            username = 'user2', email='remlover48@user2.com')
+    def setUpUsers(cls):
+        """Creates Users to be used as event creators."""
+        cls.user1 = auth_models.User.objects.create_user(
+            username='user1', email='dragonboi33@user1.com')
+        cls.user2 = auth_models.User.objects.create_user(
+            username='user2', email='remlover48@user2.com')
 
-        # event1 start/end times
-        self.start_datetime1 = timezone.now()
-        self.end_datetime1 = self.start_datetime1 + timedelta(seconds=1800)
-        self.duration1 = timedelta(seconds=1800)
-        # event2 start/end times
-        self.start_datetime2 = timezone.now() + timedelta(seconds=1800)
-        self.end_datetime2 = self.start_datetime2 + timedelta(seconds=3600)
-        self.duration2 = timedelta(seconds=3600)
+    @classmethod
+    def setUpEvents(cls):
+        """Creates Events to be used between TestCases."""
+        cls.setUpUsers()
+
+        start_datetime1 = timezone.now()
+        end_datetime1 = start_datetime1 + datetime.timedelta(seconds=1800)
+
+        cls.event1 = event_models.Event.objects.create(
+                        name='event1',
+                        creator=cls.user1,
+                        description='description1',
+                        location='location1',
+                        start_datetime=start_datetime1,
+                        end_datetime=end_datetime1)
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.setUpEvents()
--- server/events/tests/test_models.py
+++ server/events/tests/test_models.py
@@ -1,36 +1,16 @@
-from datetime import timedelta
+import datetime
+
 from django.utils import timezone
 from django.contrib.auth import models as auth_models
-from .shared_case import SharedTestCase
-from events.models import Event
 
-class EventTest(SharedTestCase):
-    """ Test module for Event model """
+from events.tests import shared_case
 
-    def setUp(self):
-        # event1
-        Event.objects.create(
-            name='event1',
-            creator=self.user1,
-            description='this is the description for event 1',
-            location='place1',
-            start_datetime=self.start_datetime1,
-            end_datetime=self.end_datetime1)
-        # event2
-        Event.objects.create(
-            name='event2',
-            creator=self.user2,
-            description='this is the description for event 2',
-            location='place2',
-            start_datetime=self.start_datetime2,
-            end_datetime=self.end_datetime2)
+class EventTest(shared_case.SharedTestCase):
+    """Tests the functionality of the Event model."""
 
     def test_event_duration(self):
-        # test event1 duration
-        expected = self.duration1
-        actual = Event.objects.get(name='event1').duration
-        self.assertEqual(expected,actual)
-        # test event2 duration
-        expected = self.duration2
-        actual = Event.objects.get(name='event2').duration
-        self.assertEqual(expected,actual)
+        """Tests the duration property of Event model."""
+        expected = datetime.timedelta(seconds=1800)
+        actual = self.event1.duration
+
+        self.assertEqual(expected, actual)
