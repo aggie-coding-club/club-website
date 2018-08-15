fc75aa9d408675cf2d2d4c31a111d0740b1d9d69
edgar-martinez <edgarmtz97@tamu.edu>
2018-08-15 14:54:57 -0500

    Registered models in admin.

--- server/events/admin.py
+++ server/events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from events import models as event_models
+
 # Register your models here.
+@admin.register(event_models.Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(event_models.WorkshopEvent)
+class WorkshopEventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(event_models.SponsoredEvent)
+class SponsoredEventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(event_models.SponsoredWorkshopEvent)
+class SponsoredWorkshopEventAdmin(admin.ModelAdmin):
+    pass
