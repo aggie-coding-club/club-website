24c16049ce32eac15756a56f397f496058f57e89
Gabriel Britain <quontas@gmail.com>
2018-08-15 14:38:20 -0500

    Removed services from notifications app; will be using notifications app as central notifications service.

--- server/projects/tests/__init__.py
+++ server/projects/tests/__init__.py
@@ -1,7 +1,6 @@
 from .models_tests import ProjectModelTests, ProjectApplicationModelTests
-from .services_tests import ServicesTest
 from .managers_tests import ProjectManagerTests, ProjectApplicationManagerTests
 from .management_commands_tests import AssignMembersToTeamsTests
 from .views_tests import ProjectViewsTests, ProjectApplicationViewsTests
-__all__ = [ProjectModelTests, ProjectApplicationModelTests, ServicesTest, ProjectManagerTests,
-           ProjectApplicationManagerTests, AssignMembersToTeamsTests, ProjectViewsTests, ProjectApplicationViewsTests]
+__all__ = ['ProjectModelTests', 'ProjectApplicationModelTests', 'ProjectManagerTests',
+           'ProjectApplicationManagerTests', 'AssignMembersToTeamsTests', 'ProjectViewsTests', 'ProjectApplicationViewsTests']
