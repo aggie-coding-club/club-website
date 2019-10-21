2d65391614647b7ee021dc64ec3bcc0ea39accf3
Conor Mai <conor.xu.mai@gmail.com>
2019-10-20 22:47:39 -0500

    Added index.html

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use("/static", express.static(path.join(__dirname, "/static")));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'static/index.html'));
+})
+
 app.listen(3000, () => {
     console.log(`Listening on port ${port}`);
 })
