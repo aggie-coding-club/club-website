2530e5c48dc90a8dc2f5ca749b9da52fd290a349
Anthony <yihonganthony@gmail.com>
2019-11-22 15:56:14 -0600

    Fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as path from 'path';
-import {officers} from './data/officers.json';
+import { officers } from './data/officers.json';
 
 const app = express();
 const port = 3000;
@@ -11,29 +11,29 @@ app.set('view engine', 'html');
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {officers: officers});
-})
+  res.render('about', { officers });
+});
 
 app.get('/announcements', (req, res) => {
-    res.render('announcements');
+  res.render('announcements');
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects');
+  res.render('projects');
 });
 
 app.get('/projects/learning', (req, res) => {
-    res.render('learning_oriented_projects.html');
+  res.render('learning_oriented_projects.html');
 });
 
 app.get('/projects/progress', (req, res) => {
-    res.render('progress_oriented_projects.html');
+  res.render('progress_oriented_projects.html');
 });
 
 app.listen(3000, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
