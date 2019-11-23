66a4a45e7b7f18a6168caf10efcc7c479714d85e
Joshua Jung <tojjung01@gmail.com>
2019-11-23 08:30:50 -0600

    Added a dummy json file for project data. Added template for project.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import * as path from 'path';
-import {officers} from './data/officers.json';
-import {projects} from './data/projects.json';
+import { officers } from './data/officers.json';
+import { projects } from './data/projects.json';
 
 const app = express();
 const port = 3000;
@@ -12,37 +12,37 @@ app.set('view engine', 'html');
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {officers: officers});
-})
-
-app.get('/announcements', (req, res) => {
-    let testCompetitions = [
-        { name: 'Discord Bot', info: 'beep boop' },
-        { name: 'Website', info: 'show them your power' },
-        { name: 'Chess', info: 'make big brain play' }
-    ] 
-
-    res.render('announcements', {
-        competitions: testCompetitions
-    });
+  res.render('about', { officers });
 });
 
-app.get('/projects', (req, res) =>{
-    res.render('projects', {projects: projects});
+app.get('/announcements', (req, res) => {
+  const testCompetitions = [
+    { name: 'Discord Bot', info: 'beep boop' },
+    { name: 'Website', info: 'show them your power' },
+    { name: 'Chess', info: 'make big brain play' },
+  ];
+
+  res.render('announcements', {
+    competitions: testCompetitions,
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects', { projects });
 });
 
 app.get('/projects/learning', (req, res) => {
-    res.render('learning_oriented_projects.html');
+  res.render('learning_oriented_projects.html');
 });
 
 app.get('/projects/progress', (req, res) => {
-    res.render('progress_oriented_projects.html');
+  res.render('progress_oriented_projects.html');
 });
 
 app.listen(3000, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
