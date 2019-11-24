06076cae0456f92cd521d91231aa4e065f12b964
George Thayamkery <george.thayamkery@gmail.com>
2019-11-24 15:53:33 -0600

    using async/await instead of promises

--- src/controllers/GitHubController.ts
+++ src/controllers/GitHubController.ts
@@ -21,48 +21,45 @@ export class GitHubControler {
    * @param repoType - Type of the repos to fetch (public, private, forks, or all)
    * @returns a list of projects belonging to this organization.
    */
-  getAllProjects(repoType?: RepoType): Promise<ProjectData[]> {
-    return this.octokit.repos
-      .listForOrg({
-        org: this.org,
-        type: repoType || 'public',
-      })
-      .then(reposResponse => {
-        const repos = reposResponse.data;
-        const projectList: ProjectData[] = [];
+  async getAllProjects(repoType?: RepoType): Promise<ProjectData[]> {
+    const reposResponse = await this.octokit.repos.listForOrg({
+      org: this.org,
+      type: repoType || 'public',
+    });
 
-        for (const repo of repos) {
-          const project: ProjectData = {
-            repoURL: repo.html_url,
-            name: repo.name,
-            description: repo.description,
-          };
-          projectList.push(project);
-        }
+    const repos = reposResponse.data;
+    const projectList: ProjectData[] = [];
 
-        return projectList;
-      })
-      .then(projectList => {
-        const apiCalls = [];
+    for (const repo of repos) {
+      const project: ProjectData = {
+        repoURL: repo.html_url,
+        name: repo.name,
+        description: repo.description,
+      };
+      projectList.push(project);
+    }
 
-        for (let i = 0; i < projectList.length; ++i) {
-          // Get tools used for each repo
-          apiCalls.push(
-            this.getProjectTopics(projectList[i].name).then(tools => {
-              projectList[i].tools = tools;
-            })
-          );
+    const apiCalls = [];
 
-          // Get contributors of each repo
-          apiCalls.push(
-            this.getProjectContributors(projectList[i].name).then(members => {
-              projectList[i].members = members;
-            })
-          );
-        }
+    for (let i = 0; i < projectList.length; ++i) {
+      // Get tools used for each repo
+      apiCalls.push(
+        this.getProjectTopics(projectList[i].name).then(tools => {
+          projectList[i].tools = tools;
+        })
+      );
+      // Get contributors for each repo
+      apiCalls.push(
+        this.getProjectContributors(projectList[i].name).then(members => {
+          projectList[i].members = members;
+        })
+      );
+    }
 
-        return Promise.all(apiCalls).then(_ => projectList);
-      });
+    // get all contributors and tools asynchronously
+    await Promise.all(apiCalls);
+
+    return projectList;
   }
 
   /**
@@ -71,20 +68,14 @@ export class GitHubControler {
    * @param projectName - Name of the repository (belonging to the org)
    * @returns a list of topics
    */
-  getProjectTopics(projectName: string): Promise<string[]> {
-    return this.octokit.repos
-      .listTopics({
-        owner: this.org,
-        repo: projectName,
-      })
-      .then(topicsResponse => {
-        const topics = topicsResponse.data;
-        const tools: string[] = [];
-        for (const topic of topics.names) {
-          tools.push(topic);
-        }
-        return tools;
-      });
+  async getProjectTopics(projectName: string): Promise<string[]> {
+    const topicsResponse = await this.octokit.repos.listTopics({
+      owner: this.org,
+      repo: projectName,
+    });
+
+    const topics = topicsResponse.data;
+    return topics.names;
   }
 
   /**
@@ -93,22 +84,22 @@ export class GitHubControler {
    * @param projectName - Name of the repository (belonging to the org)
    * @returns a list of project contributors
    */
-  getProjectContributors(projectName: string): Promise<ProjectMember[]> {
-    return this.octokit.repos
-      .listContributors({
-        owner: this.org,
-        repo: projectName,
-      })
-      .then(contributorsResponse => {
-        const contributors = contributorsResponse.data;
-        const members: ProjectMember[] = [];
-        for (const contributor of contributors) {
-          members.push({
-            profileURL: contributor.html_url,
-            imageURL: contributor.avatar_url,
-          } as ProjectMember);
-        }
-        return members;
-      });
+  async getProjectContributors(projectName: string): Promise<ProjectMember[]> {
+    const contributorsResponse = await this.octokit.repos.listContributors({
+      owner: this.org,
+      repo: projectName,
+    });
+
+    const contributors = contributorsResponse.data;
+    const members: ProjectMember[] = [];
+
+    for (const contributor of contributors) {
+      members.push({
+        profileURL: contributor.html_url,
+        imageURL: contributor.avatar_url,
+      } as ProjectMember);
+    }
+
+    return members;
   }
 }
