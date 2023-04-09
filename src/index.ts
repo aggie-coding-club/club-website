ae4986cf302a2c3363dcecf0a22237210cf443f4
ryankbr <ryan_ashk@outlook.com>
2023-04-09 01:44:05 -0500

    dont worry about it (ts)

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-function activateDarkMode() {
-  // set style to dark
-}
-
-declare global {
-  interface Window { MyNamespace: any; }
-}
